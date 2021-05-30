eb7c1eb2ae7b5e51b53e6b2b92f977b83c0222f0
Tyouxik <paulinemarie.didier@gmail.com>
2021-05-30 11:31:15 +0200

    create firebase auth helper functions

---
+++ public/helpers/firebaseAuth.js
@@ -0,0 +1,56 @@
+import firebase from "../../firebase";
+
+export const signUpwithEmailAndPassword = (formState, dispatch) => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(
+      formState.email.value,
+      formState.password.value
+    )
+    .catch((err) => {
+      dispatch({
+        type: "HANDLE_FORM_ERROR",
+        payload: { value: false, error: err.message, show: true },
+      });
+    });
+};
+
+export const signInwithEmailAndPassword = (formState, dispatch) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(formState.email.value, formState.password.value)
+    .catch((err) => {
+      console.log({ err });
+      dispatch({
+        type: "HANDLE_FORM_ERROR",
+        payload: {
+          value: false,
+          error: "Invalid credentials",
+          show: true,
+        },
+      });
+    });
+};
+
+export const signUpwithGoogle = (authDispatch) => {
+  authDispatch({ type: "LOADING" });
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().useDeviceLanguage();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .catch((error) => {
+      console.log(error);
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+
+      //TODO: "Handling account-exists-with-different-credential Errors"
+      // https://firebase.google.com/docs/auth/web/google-signin
+    });
+};
